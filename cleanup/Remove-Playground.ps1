1d061d8014d3096c5ca18be5149a3d8ac0081362
Mats Estensen <mats.estensen@devoteam.no>
2021-11-03 22:25:08 +0100

    fix: generalize ps1

--- cleanup/Remove-Playground.ps1
+++ cleanup/Remove-Playground.ps1
@@ -1,15 +1,15 @@
 #This script removes all components of the Virtual WAN Playground
 param(
-  $subscriptionID='',
-  $rgNames=@("contoso-vwan-rg", "contoso-mgmt-rg", "contoso-spoke1-rg", "contoso-onprem-rg"),
-  $cleanuptemplate='.\cleanup.json'
+  $subscriptionID = '',
+  $rgNames = @("contoso-vwan-rg", "contoso-mgmt-rg", "contoso-spoke1-rg", "contoso-onprem-rg"),
+  $cleanuptemplate = "$PSScriptRoot/cleanup.json"
 )
 
 #Select subscription
 Select-AzSubscription -SubscriptionId $subscriptionID
 
 #Remove all resources by deploying and emtpy template using Complete mode
-$jobs = foreach($rg in $rgNames) {
+$jobs = foreach ($rg in $rgNames) {
   New-AzResourceGroupDeployment -Name "cleanup-$rg" -ResourceGroupName $rg -TemplateFile .\cleanup.json -Mode Complete -Force -AsJob
 }
 
--- playground/deploy.ps1
+++ playground/deploy.ps1
@@ -1,7 +1,12 @@
-$params=@{
-    Name = 'vwan-deploy3'
-    Location = 'westeurope'
-    TemplateFile = 'C:\git\StefanIvemo\vwan-playground\playground\main.bicep'
+$timeStamp = (Get-Date -Format yyMMdd-HHmmss).ToString()
+
+$params = @{
+    Name         = "vwan-deploy-$timeStamp"
+    Location     = "westeurope"
+    TemplateFile = "$PSScriptRoot/main.bicep"
 }
+
+Write-Host "Deploying vwan-playground..."
+Write-Host "Note: This can take up to 1 hour!"
 
 New-AzSubscriptionDeployment @params -Verbose
