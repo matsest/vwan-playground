338cc71cd3851da365e1aa6c75dba5a85fa1d615
Mats Estensen <mats.estensen@devoteam.no>
2021-11-09 00:46:15 +0100

    comment on cleanup

--- cleanup/Remove-Playground.ps1
+++ cleanup/Remove-Playground.ps1
@@ -2,6 +2,8 @@
 param(
   $subscriptionID = '',
   $rgNames = @("contoso-vwan-rg", "contoso-mgmt-rg", "contoso-spoke1-rg", "contoso-onprem-rg"),
+  ## Number and names of RG's will depend on config.
+  ## Can we parse config file or do a tag-based lookup for simpler lookup? (or add explicit cleanup steps)
   $cleanuptemplate = "$PSScriptRoot/cleanup.json"
 )
 
